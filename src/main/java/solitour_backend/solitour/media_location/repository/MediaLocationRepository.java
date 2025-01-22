e26b5c67a998a6f57a33b2b7e2f0e36e98dd3afc
hyeonjaez <jhjh0022@naver.com>
2025-01-23 01:50:06 +0900

    feat: mediaType으로 랜덤으로 세개 데이터 가져오는 쿼리문

--- src/main/java/solitour_backend/solitour/media_location/repository/MediaLocationRepository.java
+++ src/main/java/solitour_backend/solitour/media_location/repository/MediaLocationRepository.java
@@ -1,12 +1,16 @@
 package solitour_backend.solitour.media_location.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import solitour_backend.solitour.media_location.entity.MediaLocation;
-import solitour_backend.solitour.tourist_spot.entity.TouristSpot;
-import solitour_backend.solitour.tourist_spot.repository.TouristSpotRepositoryCustom;
+import solitour_backend.solitour.media_location.media_type.MediaType;
 
 import java.util.List;
 
 public interface MediaLocationRepository extends JpaRepository<MediaLocation, Long>, MediaLocationRepositoryCustom {
     List<MediaLocation> findByMediaNameIn(List<String> placeNames);
+
+    @Query(value = "SELECT * FROM media_location where media_type = :mediaType ORDER BY RAND() LIMIT 3", nativeQuery = true)
+    List<MediaLocation> findRandomByMediaType(@Param("mediaType") MediaType mediaType);
 }
