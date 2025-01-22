fa5cd18186c9244a8bb79d1467b5ebcabe826fb3
hyeonjaez <jhjh0022@naver.com>
2025-01-23 01:48:37 +0900

    feat: ~님을 위한 추천 3곳 api

--- src/main/java/solitour_backend/solitour/media_location/controller/MediaController.java
+++ src/main/java/solitour_backend/solitour/media_location/controller/MediaController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,10 @@ public class MediaController {
 
     @GetMapping("/main")
     public ResponseEntity<List<MediaMainResponse>> getMediaMain(@AuthenticationPrincipal Long userId) {
-        return null;
+        List<MediaMainResponse> mediaByMain = mediaService.getMediaByMain(userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(mediaByMain);
     }
 }
